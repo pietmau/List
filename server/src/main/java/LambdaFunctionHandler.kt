97753624cbde1583b45cc9954b82870961bd07e8
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2018-10-28 18:27:15 +0000

    WIP

--- server/src/main/java/LambdaFunctionHandler.kt
+++ server/src/main/java/LambdaFunctionHandler.kt
@@ -1,61 +1,53 @@
 package com.amazonaws.lambda.demo
 
-import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.SQLException
-
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.amazonaws.services.lambda.runtime.events.S3Event
 import com.amazonaws.services.s3.AmazonS3
-import com.amazonaws.services.s3.AmazonS3ClientBuilder
-import com.amazonaws.services.s3.model.GetObjectRequest
-import com.amazonaws.services.s3.model.S3Object
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
 
-class LambdaFunctionHandler : RequestHandler<S3Event, String> {
-
+class LambdaFunctionHandler @JvmOverloads constructor(s3: AmazonS3? = null) : RequestHandler<S3Event, String> {
+    private lateinit var context: Context
+    private val logger
+        get() = context.getLogger()
     //private AmazonS3 s3 = AmazonS3ClientBuilder.standard().build();
 
-    constructor() {
-    }
-
-    // Test purpose only.
-    internal constructor(s3: AmazonS3) {
-        //  this.s3 = s3;
-    }
-
     override fun handleRequest(event: S3Event, context: Context): String {
-        //        context.getLogger().log("Received event: " + event);
-
-        val url = "jdbc:mysql://checklistappdatabase.cqe6ft379bmn.eu-west-1.rds.amazonaws.com:3306/"
-        val userName = "admin"
-        val password = "444gatti"
-        //String dbName = "checklistappdatabase";
-        val driver = "com.mysql.jdbc.Driver"
+        this.context = context
+        logger.log("Received event: " + event);
+        val connection: Connection
         try {
-            val connection = DriverManager.getConnection(url, userName, password)
-            return "Connected in Kotlin!"
-        } catch (e1: SQLException) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace()
-            return e1.localizedMessage
+            connection = openConnection()
+        } catch (exception: SQLException) {
+            return exception.localizedMessage
         }
+        //connection.
+        return  ""
+            // Get the object from the event and show its content type
+            //        String bucket = event.getRecords().get(0).getS3().getBucket().getName();
+            //        String key = event.getRecords().get(0).getS3().getObject().getKey();
+            //        try {
+            //            S3Object response = s3.getObject(new GetObjectRequest(bucket, key));
+            //            String contentType = response.getObjectMetadata().getContentType();
+            //            context.getLogger().log("CONTENT TYPE: " + contentType);
+            //            return contentType;
+            //        } catch (Exception e) {
+            //            e.printStackTrace();
+            //            context.getLogger().log(String.format(
+            //                "Error getting object %s from bucket %s. Make sure they exist and"
+            //                + " your bucket is in the same region as this function.", key, bucket));
+            //            throw e;
+            //        }
 
+    }
 
-        // Get the object from the event and show its content type
-        //        String bucket = event.getRecords().get(0).getS3().getBucket().getName();
-        //        String key = event.getRecords().get(0).getS3().getObject().getKey();
-        //        try {
-        //            S3Object response = s3.getObject(new GetObjectRequest(bucket, key));
-        //            String contentType = response.getObjectMetadata().getContentType();
-        //            context.getLogger().log("CONTENT TYPE: " + contentType);
-        //            return contentType;
-        //        } catch (Exception e) {
-        //            e.printStackTrace();
-        //            context.getLogger().log(String.format(
-        //                "Error getting object %s from bucket %s. Make sure they exist and"
-        //                + " your bucket is in the same region as this function.", key, bucket));
-        //            throw e;
-        //        }
+    private fun openConnection(): Connection {
+        val url = System.getenv("SERVER_URL")
+        val userName = System.getenv("USER")
+        val password = System.getenv("PW")
+        val driver = "com.mysql.jdbc.Driver"
+        return DriverManager.getConnection(url, userName, password)
     }
 }
---
+++ server/src/test/java/LambdaFunctionHandlerTest.kt
@@ -0,0 +1,31 @@
+import com.amazonaws.lambda.demo.LambdaFunctionHandler
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.events.S3Event
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class LambdaFunctionHandlerTest {
+    @Mock lateinit var event: S3Event
+    @Mock lateinit var context: Context
+
+    private lateinit var lambda: LambdaFunctionHandler
+
+    @Before
+    fun setUp() {
+        lambda = LambdaFunctionHandler()
+        handleRequest()
+    }
+
+    @Test
+    fun when_bar_then_fobar() {
+
+    }
+
+    private fun handleRequest() {
+        lambda.handleRequest(event, context)
+    }
+}

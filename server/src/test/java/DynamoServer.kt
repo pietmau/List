488869f481c3693fd1514cdb6aec35a4a5d4543c
pietmau <maurizio.pietrantuono@gmail.com>
2019-09-22 18:51:20 +0100

    Cleanup

--- server/src/test/java/DynamoServer.kt
+++ server/src/test/java/DynamoServer.kt
@@ -4,6 +4,8 @@ import com.pppp.travelchecklist.server.dynamodatabase.CategoriesFromTags
 import com.pppp.travelchecklist.server.dynamodatabase.DynamoCategoriesFromTags
 import com.pppp.travelchecklist.server.dynamodatabase.utils.ItemIdsFromTags
 import com.pppp.travelchecklist.server.dynamodatabase.utils.DyanmoItemIdsFromTags
+import org.assertj.core.api.Assert
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
 
@@ -15,16 +17,20 @@ class DynamoServer {
     @Test
     fun `when get items id by tags returns ids`() {
         val itemIds = itemIdsFromTags.getItemIdsFromTags(tags)
+        assertThat(itemIds).hasSize(2).contains("1", "2")
     }
 
     @Test
     fun `get categoryes by tag`() {
         val categories = categoriesFromTags.getCategoriesBasedOnTags(tags)
+        val titles = categories.map { it.title }
+        assertThat(titles).hasSize(2).contains("Clothes", "Dressy clothes")
     }
 
     @Test
     fun `get categoryes by tag 1`() {
         val tags = mutableListOf<Tag>(ServerTag(id = "1", title = ""))
         val categories = categoriesFromTags.getCategoriesBasedOnTags(tags)
+        assertThat(categories).hasSize(1)
     }
 }
--- uploader/src/main/java/com/pppp/uploader/UploaderComponent.kt
+++
@@ -1,8 +0,0 @@
-package com.pppp.uploader
-
-import dagger.Component
-
-@Component(modules = arrayOf(Module::class))
-interface UploaderComponent {
-    fun inject(mainActivity: MainActivity)
-}

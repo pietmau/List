0121a1ab04226a6f0c22a98873a45805b68c791e
pietmau <maurizio.pietrantuono@gmail.com>
2019-11-23 21:49:08 +0000

    Add dialog

--- app/src/main/java/com/pppp/travelchecklist/edititem/EditItemDialogFragment.kt
+++ app/src/main/java/com/pppp/travelchecklist/edititem/EditItemDialogFragment.kt
@@ -1,8 +1,9 @@
 package com.pppp.travelchecklist.edititem
 
 import android.app.Dialog
-import android.content.DialogInterface
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import com.pppp.travelchecklist.R
@@ -11,6 +12,7 @@ import com.pppp.travelchecklist.utils.appComponent
 import javax.inject.Inject
 
 class EditItemDialogFragment : DialogFragment() {
+    private lateinit var alertDialog: AlertDialog
     @Inject
     lateinit var presenter: EditItemDialogFragmentPresenter
 
@@ -21,15 +23,24 @@ class EditItemDialogFragment : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val builder = AlertDialog.Builder(requireActivity())
-        builder.apply {
+        with(builder) {
             setPositiveButton(R.string.ok,
-                DialogInterface.OnClickListener { dialog, id ->
+                { _, id ->
                 })
             setNegativeButton(R.string.cancel,
-                DialogInterface.OnClickListener { dialog, id ->
+                { _, id ->
                 })
+            setTitle(R.string.editItem)
+            setView(getLayout())
         }
-        return builder.create()
+        alertDialog = builder.create()
+        return alertDialog
+    }
+
+    private fun getLayout() = R.layout.custom_dialog_layout
+
+    private fun newView(): View {
+        return TextView(requireContext()).apply { text = "dsdasd" }
     }
 
     companion object {
